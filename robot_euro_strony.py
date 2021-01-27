1967b16958e609a558fd438001137dbee2450457
Paskud4 <kaspipiorkow@gmail.com>
2021-01-27 02:42:06 +0100

    Initial commit

---
+++ robot_euro_strony.py
@@ -0,0 +1,114 @@
+"""
+MM ma zabezpieczenia przeciw botom
+euro działa
+
+pobieram :
+        * producent
+        * model
+        * wielkość
+        * rozdzielczość
+        * smartTV
+        1 id
+        2 name
+        3 brand
+        7 price
+        8 link
+"""
+import json5
+import re
+import requests
+from bs4 import BeautifulSoup as bs
+
+import pandas as pd
+
+
+class Robot:
+
+    def __init__(self):
+        self.url_euro_tv_beg = 'https://www.euro.com.pl/telewizory-led-lcd-plazmowe,strona-'
+        self.url_euro_tv_end = '.bhtml'
+        self.url_euro_base = 'https://www.euro.com.pl'
+        self.licznik_niedostepnych = 0
+
+    def GetPagesCount(self) -> int:
+        page_soup = self.soup_euro.find("div", { "class" : "paging-numbers"})
+        page_list = page_soup.find_all("a")
+        last_page = len(page_list) - 1
+        return int(page_list[last_page].text)
+
+    def _Connect_wPage(self, page_num) -> bool:
+        try:
+            self.respond_euro = requests.get(self.url_euro_tv_beg + str(page_num) + self.url_euro_tv_end)
+            self.soup_euro = bs(self.respond_euro.content, 'html.parser')
+            return True
+        except:
+            return False
+
+    def _GetProductData(self, soup):
+        param_table = soup.find("table", {"class": "description-tech-details js-tech-details"})
+
+        params = param_table.find_all("td")
+
+        s_size = params[3].text.replace("\t", "").replace("\n", "")
+        resolution = params[5].text.replace("\t", "").replace("\n", "")
+        matrixType = params[11].text.replace("\t", "").replace("\n", "")
+        smartTV = params[39].text.replace("\t", "").replace("\n", "")
+        return s_size, resolution, matrixType, smartTV
+
+    def _CollectDataFromPage(self, num):
+        list = []
+        if self._Connect_wPage(num) == True:
+            product_list = self.soup_euro.find("div", {"id": "product-list"})
+            product_script = product_list.find_all("script", {"type": "text/javascript"})
+
+            norm = str(product_script).replace("\t", "").replace("\n", "")
+            data = re.search(r'\((.*?)\)', norm)
+
+            try:
+                js_script = json5.loads(data.group(1))
+                js_prod = json5.loads(js_script['products'])
+                for tv in js_prod:
+                    js = json5.loads(tv)
+                    brand = js['brand']
+                    name = js['name']
+                    price = js['price']
+                    id = js['id']
+
+                    # LINK
+                    link = js['link']
+                    new_resp = requests.get(url=self.url_euro_base + link)
+                    soup = bs(new_resp.content, 'html.parser')
+
+                    s_size, resolution, matrixType, smartTV = self._GetProductData(soup)
+
+                    list.append([brand, name, price, id, s_size, resolution, matrixType, smartTV])
+            except:
+                self.licznik_niedostepnych += 1
+        else:
+            pass
+
+        return list
+
+    def CollectData_euro(self):
+        list = []
+        if self._Connect_wPage(1) == False:
+            return list
+        pages_num = self.GetPagesCount()
+
+        for num in range(1, pages_num):
+            print(num)
+            list += self._CollectDataFromPage(num)
+
+        return list
+
+
+if __name__ == '__main__':
+    rb = Robot()
+    list = rb.CollectData_euro()
+    print(len(list))
+    print(list)
+
+    data = pd.DataFrame(list)
+    print(data)
+    print(data.shape)
+
---
+++ robot_mm_strony.py
@@ -0,0 +1,95 @@
+import json5
+import re
+import requests
+from bs4 import BeautifulSoup as bs
+from fake_useragent import UserAgent
+
+import pandas as pd
+
+class Robot:
+
+    def __init__(self):
+        self.url_media_beg = "https://mediamarkt.pl/rtv-i-telewizory/telewizory?page="
+        self.url_media_base = "https://mediamarkt.pl"
+        #self.respond = requests.get(self.url_media)
+
+        # Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25
+        ua = UserAgent()
+        self.header = {'User-Agent': str(ua.safari)}
+
+    def GetPagesCount(self) -> int:
+        self._Connect_wPage(1)
+        page_soup = self.soup_mm.find("span", {"class" : "m-pagination_count"})
+        pages_num = int(str(page_soup.text).replace("z", ""))
+        return pages_num
+
+    def _Connect_wPage(self, page_num) -> bool:
+        try:
+            self.respond_mm = requests.get(self.url_media_beg + str(page_num), headers=self.header)
+            self.soup_mm = bs(self.respond_mm.content, 'html.parser')
+            return True
+        except:
+            return False
+
+    def _GetProductData(self, link):
+        try:
+            new_resp = requests.get(url=link, headers = self.header)
+            soup = bs(new_resp.content, 'html.parser')
+            param_tables = soup.find("div", {"class" : "m-offerShowData"})
+            param_table = param_tables.find_all("div", {"class" : "m-offerShowData_item js-offerShowData_item js-offerShowData"})
+            featuresTech = param_table[0].find_all("span")
+            featuresScr = param_table[1].find_all("span")
+            featuresBrand = param_table[8].find_all("a")
+            matrixType = str(featuresTech[1].text)
+            smartTV = str(featuresTech[3].text)
+            size = str(featuresScr[1].text)
+            resolution = str(featuresScr[9].text)
+            resolutionName = str(featuresScr[7].text)
+            brand = str(featuresBrand[0].text)
+            title = soup.find("h1", {"class" : "b-ofr_headDataTitle"})
+            name = str(title.text).replace("Telewizor ", "")
+            param_price_box = soup.find("div", {"class" : "b-contentSideBox"})
+            param_price = param_price_box.find("div", {"class" : "m-priceBox_price"})
+            price = str(param_price.text).replace(" ", "").replace(",-zł","").replace("\n", "")
+
+            return brand, name, price, size, resolution, resolutionName, matrixType, smartTV
+        except:
+            pass
+
+    def _CollectDataFromPage(self, num):
+        list = []
+        product_list = self.soup_mm("div", {"class": "b-row clearfix2 b-listing_classic js-eqContainer js-offerBox js-equalHRow"})
+        product = product_list[0].find_all("div", {"class": "m-offerBox_content"})
+
+        for p in product:
+            name_scr = p.find("div", {"class": "m-offerBox_box"})
+            name_a = name_scr.find("a")
+            link = self.url_media_base + name_a.get('href')
+            brand, name, price, size, resolution, resolutionName, matrixType, smartTV = self._GetProductData(link)
+            list += [[brand, name, price, size, resolution, resolutionName, matrixType, smartTV]]
+        return list
+
+    def CollectData_mm(self) -> pd.DataFrame:
+        list = []
+        if self._Connect_wPage(1) == False:
+            return list
+        pages_num = self.GetPagesCount()
+        pages_num = 3
+
+        for num in range(1, pages_num):
+            print(num)
+            list += self._CollectDataFromPage(num)
+
+        return list
+
+if __name__ == '__main__':
+    rb = Robot()
+    x = rb.GetPagesCount()
+    print(x)
+    list = rb.CollectData_mm()
+    print(len(list))
+    print(list)
+
+    data = pd.DataFrame(list)
+    print(data)
+    print(data.shape)
